ad8c81b799091e3882107fc04235909e04ece387
sethzento-web <sethzento@gmail.com>
2026-02-25 10:55:58 +0700

    message

--- script.js
+++ script.js
@@ -38,3 +38,13 @@ function loginUser(event) {
         window.location = "admin.html";
     }
 }
+
+function togglePassword() {
+    var passwordInput = document.getElementById("password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
