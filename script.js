7cd69afd0efa13efef14e66f582537f34684dbf5
sethzento-web <sethzento@gmail.com>
2026-02-25 10:29:59 +0700

    message

---
+++ script.js
@@ -0,0 +1,40 @@
+function checkPassword() {
+    var password = document.getElementById("password").value;
+    var feedback = document.getElementById("feedback");
+
+    var rules = 0;
+
+    if (password.length >= 8) {
+        rules++;
+    }
+
+    if (/[0-9]/.test(password)) {
+        rules++;
+    }
+
+    if (/[!@#$%^&*]/.test(password)) {
+        rules++;
+    }
+
+    if (rules <= 1) {
+        feedback.innerHTML = "Weak Password";
+        feedback.style.color = "red";
+    } else if (rules == 2) {
+        feedback.innerHTML = "Medium Password";
+        feedback.style.color = "orange";
+    } else {
+        feedback.innerHTML = "Strong Password";
+        feedback.style.color = "green";
+    }
+}
+
+function loginUser(event) {
+    event.preventDefault();
+    var role = document.getElementById("role").value;
+
+    if (role == "nurse") {
+        window.location = "nurse.html";
+    } else {
+        window.location = "admin.html";
+    }
+}
